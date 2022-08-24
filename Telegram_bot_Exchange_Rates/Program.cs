1f011c839689c15d1ae82de73d8051984de4be2a
oleksikoleso@gmail.com <oleksikoleso@gmail.com>
2022-08-24 22:55:22 +0100

    W

--- Telegram_bot_Exchange_Rates/Program.cs
+++ Telegram_bot_Exchange_Rates/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Telegram.Bot;
+using Telegram.Bot.Exceptions;
 using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
@@ -6,12 +7,23 @@ using Telegram.Bot.Types.Enums;
 var botClient = new TelegramBotClient("5355422609:AAGVFUA8bIup8ihBssW7y2weZst_jtm-Phs");
 
 using var cts = new CancellationTokenSource();
-var reservingOption = new ReceiverOptions()
+var receiverOptions = new ReceiverOptions()
 {
     AllowedUpdates = Array.Empty<UpdateType>()
 };
 
+botClient.StartReceiving(
+    updateHandler: HandleUpdateAsync,
+    pollingErrorHandler: HandlePollingErrorAsync,
+    receiverOptions: receiverOptions,
+    cancellationToken: cts.Token
+    );
 
+var me = await botClient.GetMeAsync();
+Console.WriteLine($"Start listening for @{me.Username}");
+
+
+Console.ReadLine();
 cts.Cancel();
 
 async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
@@ -30,4 +42,16 @@ async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, Cancel
         text:$"You said:\n '{messageText}'",
         cancellationToken: cancellationToken);
 
+}
+ Task HandlePollingErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+    var ErrorMessage = exception switch
+    {
+        ApiRequestException apiRequestException
+            => $"Telegram API Error:\n[{apiRequestException.ErrorCode}]\n{apiRequestException.Message}",
+           _ => exception.ToString()
+    };
+
+    Console.WriteLine(ErrorMessage);
+    return Task.CompletedTask;
 }
