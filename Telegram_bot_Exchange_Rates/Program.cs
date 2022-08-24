479bcbb680edcfac6897d976a1f769e5235fb021
oleksikoleso@gmail.com <oleksikoleso@gmail.com>
2022-08-24 22:11:43 +0100

    Добавьте файлы проекта.

---
+++ Telegram_bot_Exchange_Rates/Program.cs
@@ -0,0 +1,33 @@
+﻿using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+var botClient = new TelegramBotClient("5355422609:AAGVFUA8bIup8ihBssW7y2weZst_jtm-Phs");
+
+using var cts = new CancellationTokenSource();
+var reservingOption = new ReceiverOptions()
+{
+    AllowedUpdates = Array.Empty<UpdateType>()
+};
+
+
+cts.Cancel();
+
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is not { } message)
+        return;
+    if (message.Text is not { } messageText)
+        return;
+
+    var chatId = message.Chat.Id;
+
+   Console.WriteLine($"Resived '{messageText}' mesasage in chat {chatId}");
+
+    Message sentMessage = await botClient.SendTextMessageAsync(
+        chatId: chatId,
+        text:$"You said:\n '{messageText}'",
+        cancellationToken: cancellationToken);
+
+}
